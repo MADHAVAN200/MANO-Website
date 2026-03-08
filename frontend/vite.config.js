15e663db922f13572e0014d57d79bfe7865ec234
MADHAVAN200 <madhavannadar23@gmail.com>
2026-03-08 17:31:16 +0530

    Making responsive website -> basic dependencies done

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,6 +3,14 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   base: '/',
+  server: {
+    host: true,
+    port: 5173,
+  },
+  preview: {
+    host: true,
+    port: 5173,
+  },
   build: {
     assetsDir: 'static',
   },
