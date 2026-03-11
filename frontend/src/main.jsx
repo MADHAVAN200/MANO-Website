8936473d80c3bf36bc1744742152604c99b24a62
MADHAVAN200 <madhavannadar23@gmail.com>
2026-03-12 00:58:16 +0530

    Packages sorted for the tablet view

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,10 +2,13 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import 'material-icons/iconfont/material-icons.css'
 import App from './App.jsx'
+import { ToastProvider } from './components/Toast.jsx'
 import './index.css';
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter basename={import.meta.env.BASE_URL}>
-    <App />
+    <ToastProvider>
+      <App />
+    </ToastProvider>
   </BrowserRouter>
 )
